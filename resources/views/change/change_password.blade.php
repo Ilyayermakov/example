9b80306df5d773377b96bced46b7cb2474683d0c
Ilyayermakov <125358960+Ilyayermakov@users.noreply.github.com>
2023-09-29 15:43:37 +0200

    Update change_password.blade.php

--- resources/views/change/change_password.blade.php
+++ resources/views/change/change_password.blade.php
@@ -4,6 +4,8 @@
 
 <x-title-table>
     <x-form method="POST" action="{{ route('updatePassword') }}">
+        <h5>{{__('Введите код полученый в письме:')}}</h5>
+        <x-input type="text" name="code" />
         <h5>{{__('Введите новый пароль:')}}</h5>
         <x-input type="password" name="new_password" />
         <h5>{{__('Повторите новый пароль:')}}</h5>
